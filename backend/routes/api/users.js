4087ea10580c4758afaeab357ecfe8094c32401b
Huey Nguyen <syndrumablez@gmail.com>
2023-04-13 20:03:41 -0700

    testing routes with firstName and lastName

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,13 +28,15 @@ const validateSignup = [
 
 // Sign up
 router.post("", validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
+  const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, username, hashedPassword });
+  const user = await User.create({ email, firstName, lastName, username, hashedPassword });
 
   const safeUser = {
     id: user.id,
     email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
     username: user.username,
   };
 
