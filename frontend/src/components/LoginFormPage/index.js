85bf82dc2ed1008929a425a16e5cbc4c3d143dbe
Huey Nguyen <syndrumablez@gmail.com>
2023-05-25 20:45:37 -0700

    frontend-login

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+import "./LoginForm.css";
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="credential">Username or Email</label>
+          <input
+            type="text"
+            id="credential"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {errors.credential && <p className="error-message">{errors.credential}</p>}
+        <button type="submit" className="login-button">Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from './csrf';
+
+// Action Type
+const SET_SESSION_USER = 'session/SET_SESSION_USER';
+const CLEAR_SESSION_USER = 'session/CLEAR_SESSION_USER';
+
+// Action Creators
+const setSessionUser = (user) => ({
+  type: SET_SESSION_USER,
+  payload: user,
+})
+
+const clearSessionUser = () => ({
+  type: CLEAR_SESSION_USER,
+})
+
+// Thunk action creator
+export const login = (user) => async dispatch => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setSessionUser(data.user));
+    return response;
+  }
+};
+
+// Initial state
+const initialState = {
+  user: null,
+};
+
+// Reducer
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_SESSION_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case CLEAR_SESSION_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
